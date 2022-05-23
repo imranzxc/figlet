47cc9fa013b311df710d4a6dd426c032a22a9fb0
Imran <imran.bursagov@gmail.com>
2022-05-23 15:52:12 +0300

    imran

---
+++ index.js
@@ -0,0 +1,5 @@
+var  figlet  =  require ( 'figlet' ) ;
+
+figlet ( 'intocode' ,  function ( err ,  data )  { 
+    if  ( err )  { 
+        console.log ( ' Что - то пошло не так ... ' ) ; console.dir ( err ) ; return ; } console.log ( data ) } ) ;
